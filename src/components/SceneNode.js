94c468ef1678bb2b61fba947948aa1151ff90faa
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2023-02-09 20:45:43 +0100

    update dependencies

--- src/components/SceneNode.js
+++ src/components/SceneNode.js
@@ -1,39 +1,39 @@
 import { OrbitControls } from "@react-three/drei";
-import { useDrag } from "react-use-gesture";
+import { useDrag } from "@use-gesture/react"
 const { useThree } = require("@react-three/fiber");
 const { useRef, useState } = require("react");
 
 
 function SceneNode(props) {
-    const ref = useRef();
+  const ref = useRef();
 
-    const [position, setPosition] = useState(props.position)
+  const [position, setPosition] = useState(props.position)
 
-    const { size, viewport } = useThree()
-    const aspect = (size.width / viewport.width)
+  const { size, viewport } = useThree()
+  const aspect = (size.width / viewport.width)
 
-    var bind = useDrag(
-        ({ offset: [xOffset, yOffset] }) => {
-          const [x, y, z] = props.position;
-          setPosition([x, y - (yOffset / aspect), z - (xOffset/aspect)]);
-        },
-        { pointerEvents: true }
-      );
-    
-    if (!props.customDrag) {
-      bind = () => {}
-    }
+  var bind = useDrag(
+    ({ offset: [xOffset, yOffset] }) => {
+      const [x, y, z] = props.position;
+      setPosition([x, y - (yOffset / aspect), z - (xOffset / aspect)]);
+    },
+    { pointerEvents: true }
+  );
 
-    return (
-        <mesh
-            {...props}
-            position={position}
-            {...bind()}
-            ref={ref}
-            onClick={() => console.log(ref)}
-        >
-        </mesh>
-    )
+  if (!props.customDrag) {
+    bind = () => { }
+  }
+
+  return (
+    <mesh
+      {...props}
+      position={position}
+      {...bind()}
+      ref={ref}
+      onClick={() => console.log(ref)}
+    >
+    </mesh>
+  )
 }
 
 
