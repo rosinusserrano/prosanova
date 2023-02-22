9f583e473e505cf364d88453cbc64089cac75cc1
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2023-02-22 15:49:23 +0100

    add strictmode to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Imprint from './components/Imprint';
 
 import WholeSceneAsNodes from './components/WholeSceneAsNodes';
 import WholeScene from './components/WholeScene';
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/fonts.css';
 import './styles/imprint.css';
@@ -19,39 +19,41 @@ import { Canvas } from '@react-three/fiber';
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route index element={
-          <div className='App'>
-            <Imprint />
-            <header className='App-header'>
-              <WholeSceneAsNodes />
-            </header>
-          </div>
-        } />
-        <Route path="/imprint" element={<ImprintPage />} />
-        <Route path="/ref" element={
-          <div className='App'>
-            <Imprint />
-            <header className='App-header'>
-              <WholeScene />
-            </header>
-          </div>}>
-        </Route>
-        <Route path="/test" element={
-          <div className='App'>
-            <Imprint />
-            <header className='App-header'>
-              <Canvas style={{ height: "100vh", width: "100vw" }}>
-                <ambientLight></ambientLight>
-                <pointLight position={[-1.5, -.5, 4]}></pointLight>
-                <Test />
-              </Canvas>
-            </header>
-          </div>}>
-        </Route>
-      </Routes>
-    </Router>
+    <StrictMode>
+      <Router>
+        <Routes>
+          <Route index element={
+            <div className='App'>
+              <Imprint />
+              <header className='App-header'>
+                <WholeSceneAsNodes />
+              </header>
+            </div>
+          } />
+          <Route path="/imprint" element={<ImprintPage />} />
+          <Route path="/ref" element={
+            <div className='App'>
+              <Imprint />
+              <header className='App-header'>
+                <WholeScene />
+              </header>
+            </div>}>
+          </Route>
+          <Route path="/test" element={
+            <div className='App'>
+              <Imprint />
+              <header className='App-header'>
+                <Canvas style={{ height: "100vh", width: "100vw" }}>
+                  <ambientLight></ambientLight>
+                  <pointLight position={[-1.5, -.5, 4]}></pointLight>
+                  <Test />
+                </Canvas>
+              </header>
+            </div>}>
+          </Route>
+        </Routes>
+      </Router>
+    </StrictMode>
   );
 
 }
