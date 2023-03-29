30a621caf6217fdeb4a68cf2370fe76cab7f46c4
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2023-03-29 18:22:56 +0200

    added pn20 button back in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,15 @@ import '../styles/imprint.css';
 import '../styles/fonts.css';
 import '../styles/pages.css';
 
-export default function Navbar(){
+export default function Navbar() {
 
-    return(
-    <div className='navbar'>
+    return (
+        <div className='navbar'>
 
-<Link className='button' target='_blank' to='https://www.instagram.com/prosanovafestival/'>Instagram</Link>
+            <Link className='button' target='_blank' to='https://www.instagram.com/prosanovafestival/'>Instagram</Link>
+            <Link className='button' target='_blank' to='https://prosanova-festival.net'>pn20</Link>
 
-    </div>
+        </div>
 
 
     )
