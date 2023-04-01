77118037ae839cf09b8e7e2eb3f40efef2a0a9f3
yaelrealdog <57135510+yaelrealdog@users.noreply.github.com>
2023-04-01 15:48:21 +0200

    Termine und sonstige kleinigkeiten

--- src/components/Termine.js
+++ src/components/Termine.js
@@ -18,13 +18,132 @@ function Termine() {
       </Helmet>
 
       <div className=" page-grid-inside-5-cl  ">
-        <div className="    font-size-1 grid-6-column-mobile grid-1-column  grid-child  ">
-        <div className="inside-page-padding termine-Datum border-radius font-size-3">30/3</div>
-        <div className="inside-page-padding border-radius termine-titel font-size-3 font-script">Bekenntnisse an das Misstrauen</div>
+        <div className="  grid-tab-column-1  font-size-1 grid-6-column-mobile grid-1-column  grid-child  ">
+          <div className="inside-page-padding termine-Datum border-radius font-size-3">
+            30/03
+          </div>
 
+          <div className="inside-page-padding border-radius termine-titel font-size-4 font-script">
+            Bekenntnisse an das Misstrauen
+          </div>
         </div>
 
+        <div className="  grid-tab-column-1    font-size-1 grid-6-column-mobile grid-1-column  grid-child  ">
+          <div className="inside-page-padding termine-Datum border-radius font-size-3">
+            11/05
+          </div>
 
+          <div className="inside-page-padding border-radius termine-titel font-size-4 font-script">
+            Auftakt
+            <br></br> Lesung
+          </div>
+        </div>
+
+        <div className="   grid-tab-column-1   font-size-1 grid-6-column-mobile grid-1-column  grid-child  ">
+          <div className="inside-page-padding termine-Datum border-radius font-size-3">
+            23/05
+          </div>
+
+          <div className="inside-page-padding border-radius termine-titel font-size-4 font-script">
+            Abend der <br></br>
+            Kollektive
+          </div>
+        </div>
+
+        <div className="   grid-tab-column-1   font-size-1 grid-6-column-mobile grid-1-column  grid-child  ">
+          <div className="inside-page-padding termine-Datum border-radius font-size-3">
+            23–25/06
+          </div>
+
+          <div className="inside-page-padding border-radius termine-titel font-size-4 font-script">
+            Festival<br></br>
+            Wochenende
+          </div>
+        </div>
+
+        <div className="   grid-tab-column-1   font-size-1 grid-6-column-mobile grid-1-column  grid-child  ">
+          <div className="inside-page-padding termine-Datum border-radius font-size-3">
+            15/08
+          </div>
+
+          <div className="inside-page-padding border-radius termine-titel font-size-4 font-script">
+            Sonderaugabe der <br></br> Bella Triste
+          </div>
+        </div>
+
+        <div className="grid-6-column-mobile mobile-grid-row-start-2 termine-text background-color-grey inside-page-padding border-radius scrollable-element grid-tab-column-1 grid-1-column">
+          OPEN CALL: Bekenntnisse an das Misstrauen Zu welchem Misstrauen
+          bekennt ihr euch? <br></br><br></br>Zu dieser Frage suchen wir eure szenischen,
+          experimentellen und gattungsbrechenden Texte, die thematisch aus
+          unterschiedlichen Kontexten stammen können. Drei Texte werden mit
+          einem Preisgeld ausgezeichnet und auf dem Festival inszeniert. Die
+          Jury besteht aus Olufemi Atibioke, Deniz Bolat und Laura Shirin, die
+          euch im Anschluss an die Auswahlphase bei der Umsetzung eurer
+          Texteinreichungen und Konzepte sowohl inhaltlich, als auch
+          dramaturgisch zur Seite stehen werden. Wie die Texte umgesetzt werden,
+          bleibt euch dabei in Absprache mit den Mentor*innen freigestellt - ob
+          klassisch, experimentell, visuell, interaktiv, medial oder etwas
+          anderes. Natürlich solange es sich im vorgegebenen Budget bewegt. Die
+          Teilnahme an einem Probenwochenende mit den Mentor*innen vor dem
+          Festival ist verpflichtend. Eure Einsendungen könnt ihr als
+          PDF-Dateien bis zum 30.03.2023 an presse@prosanova.net schicken, gerne
+          auch mit Konzept, wie ihr euch eure Inszenierung/Umsetzung beim
+          Festival vorstellen könnt. Der Open Call richtet sich an Menschen mit
+          Wohnsitz in Deutschland. Fragen an besagte E-Mail-Adresse oder per
+          Instagram.
+        </div>
+        <div className="grid-6-column-mobile mobile-grid-row-start-4 termine-text background-color-grey inside-page-padding border-radius scrollable-element grid-tab-column-1 grid-1-column">
+          Auftaktlesung im Literaturhaus St. Jakobi <br></br><br></br>Literaturhaus St. Jakobi,
+          12345 Hildesheim, 19:30 <br></br><br></br> Über einen Monat vor dem Festivalwochenende
+          starten wir mit einer ersten Lesung. Heike Geißler, Bezhad Karim Khani
+          und Karosh Taha sind zu Gast und lesen für uns aus neuen und bekannten
+          Büchern. Wir wollen uns auf das Festival einstimmen und ersten Worten
+          lauschen. Begleitet wird das Ganze vom NDR. Mehr Informationen und
+          Tickets hier:
+          <Link className="Links" to=" https://stjakobi.de/veranstaltungen/prosanova-auftakt-mit-behzad-karim-khani-karosh-taha-und-heike-geissler">
+            {" "}
+            https://stjakobi.de/veranstaltungen/prosanova-auftakt-mit-behzad-karim-khani-karosh-taha-und-heike-geissler
+          </Link>
+        </div>
+
+        <div className="grid-6-column-mobile mobile-grid-row-start-6 termine-text background-color-grey inside-page-padding border-radius scrollable-element grid-tab-column-1 grid-1-column">
+          Und dazwischen Zwischenräume. Abend der Kollektive Literaturhaus <br></br><br></br>
+          St.Jakobi, 12345 Hildesheim, 18:30  <br></br><br></br>In einer Kooperation mit dem
+          Literaturinstitut der Universität Hildesheim und dem Literaturhaus St.
+          Jakobi widmen wir uns dem Kollektiven als Praxis im Literaturbetrieb.
+          Wie originell können wir sein, wenn wir gemeinsam arbeiten? Welche
+          Rolle nimmt die soziale Praxis neben der künstlerischen ein und wie
+          viele Kompromisse müssen eingegangen werden? Welche Ausschlüsse
+          geschehen, wenn wir uns auf ganz bestimmte Kollektivpartner:innen
+          einlassen? All das wollen wir uns gemeinsam fragen - und holen uns
+          dafür Unterstützung von einzelnen Vertreter:innen aus verschiedenen
+          Kollektiven. Mit dabei: Lynn Takeo Musiol (dyke dogs), Kyra Schmidt
+          (Biwi Kefempom), Sandra Burckhardt (Wiese), Tristan Marquardt (Q13),
+          Alexnder Rupflin (Hermes Baby) und Maren Wurster. Der Fokus liegt an
+          diesem Abend auf dem Gemeinsamen - wir wollen diskutieren und so
+          selbst kollektive Erfahrungen machen. Mehr Informationen und Tickets
+          hier:
+        </div>
+        <div className="grid-6-column-mobile mobile-grid-row-start-8 termine-text background-color-grey inside-page-padding border-radius scrollable-element grid-tab-column-1 grid-1-column">
+          Festivalwochenende  23.–25.06.  <br></br><br></br>Hildesheim  <br></br><br></br>Seid dabei, wenn das
+          Prosanova-Festival in die siebte Runde geht und macht Hildesheim mit
+          uns unsicher. Literatur, Performance, Musik, Workshops und das
+          Get-Together des Literaturbetriebs wartet in Hildesheim auf euch!
+        </div>
+        <div className="grid-6-column-mobile mobile-grid-row-start-10 termine-text background-color-grey inside-page-padding border-radius scrollable-element grid-tab-column-1 grid-1-column">
+          Sonderausgabe der BELLA triste – Zeitschrift für junge Literatur <br></br><br></br>
+          15.08.  <br></br><br></br>Hildesheim In der Sondersausgabe der BELLA triste widmen wir
+          uns dem Archivieren als künstlerischer Praxis. Wie können wir
+          festhalten, was wir an drei Tagen Festival erlebt haben? Was bleibt
+          vom Rausch? Wir wollen die BELLA als Ort der Versammlung sehen, als
+          Begegnungsstätte und weniger als bloße Dokumentation des PROSANOVAs.
+          Wir fragen uns, wie wir Strukuren weiterführen können, die wir auf dem
+          Festival erprobt haben. Ist es möglich, das PROSANOVA zwischen zwei
+          Buchdeckel zu klemmen? In der "Schlechte Wörter"-BELLA werden Texte,
+          Fotos sowie alles dazwischen veröffentlicht, die in irgendeiner Art
+          und Weise Teil des Festivals waren. Dazu arbeiten wir eng mit der
+          Redaktion der BELLA triste zusammen.
+        </div>
       </div>
     </>
   );
