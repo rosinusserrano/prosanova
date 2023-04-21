20f3071e97351bca2077d94b68a7d62ddf0e68fa
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2023-04-21 12:44:05 +0200

    stashed012345

--- src/components/Header/HeaderSmall.js
+++ src/components/Header/HeaderSmall.js
@@ -1,13 +1,118 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import { useWindowSize } from 'hooks/useWindowSize';
+import React, { useState } from 'react'
 
-function HeaderSmall({setActivePage}) {
+function HeaderSmall({ setActivePage }) {
+    const [expanded, setExpanded] = useState(false);
+
     return (
         <header>
-            <div className='grid-column-start-1 background-color-grey' style={{height: "30px", width: "30px"}}></div>
-            <div className="Logo-Header-Wrapper grid-column-start-5">
-                <div>  <img src={'images/PROSANOVA-Logo-Klein.png'} alt="Logo Small" onClick={() => setActivePage("game")} /></div>
+            <div className="collapsed-header-grid">
+                <div style={{ gridArea: "overlay" }}>
+                    <div className='background-color-grey'
+                        style={{
+                            height: "30px",
+                            width: "30px",
+                        }}
+                        onClick={() => setExpanded(!expanded)}></div>
+                    <div className="Logo-Header-Wrapper">
+                        <div>
+                            <img src={'images/PROSANOVA-Logo-Klein.png'} alt="Logo Small" onClick={() => setActivePage("game")} />
+                        </div>
+                    </div>
+                </div>
+                <motion.div layout variants={{
+                    collapsed: {
+                        width: "100%",
+                        height: 0
+                    },
+                    expanded: {
+                        width: "100%",
+                        height: "100%"
+                    }
+                }}
+                    initial={"collapsed"}
+                    animate={expanded ? "expanded" : "collapsed"}
+                    style={
+                        {
+                            // gridColumnStart: 1,
+                            // gridRow: 1,
+                            // gridColumnEnd: 5,
+                            gridArea: "overlay",
+                            pointerEvents: "none",
+                            zIndex: 20,
+                            overflow: "clip",
+                            textOverflow: "clip"
+                        }
+                    }
+                    className='background-color-grey grid-5-column'>
+                    <p>
+                        thh
+                        ddwd
+                        dasd
+                        asdsadas
+                        sadasdasd
+                        asdsadasdsasdasd
+                        sadasdasdsadas
+                        asdsadasdsasdasd
+                        asdsadas
+                        thh
+                        ddwd
+                        dasd
+                        asdsadas
+                        sadasdasd
+                        asdsadasdsasdasd
+                        sadasdasdsadas
+                        asdsadasdsasdasd
+                        asdsadas
+                        thh
+                        ddwd
+                        dasd
+                        asdsadas
+                        sadasdasd
+                        asdsadasdsasdasd
+                        sadasdasdsadas
+                        asdsadasdsasdasd
+                        asdsadas
+                        thh
+                        ddwd
+                        dasd
+                        asdsadas
+                        sadasdasd
+                        asdsadasdsasdasd
+                        sadasdasdsadas
+                        asdsadasdsasdasd
+                        asdsadas
+                        thh
+                        ddwd
+                        dasd
+                        asdsadas
+                        sadasdasd
+                        asdsadasdsasdasd
+                        sadasdasdsadas
+                        asdsadasdsasdasd
+                        asdsadas
+                        thh
+                        ddwd
+                        dasd
+                        asdsadas
+                        sadasdasd
+                        asdsadasdsasdasd
+                        sadasdasdsadas
+                        asdsadasdsasdasd
+                        asdsadas
+                        thh
+                        ddwd
+                        dasd
+                        asdsadas
+                        sadasdasd
+                        asdsadasdsasdasd
+                        sadasdasdsadas
+                        asdsadasdsasdasd
+                        asdsadas
+                    </p>
+                </motion.div>
             </div>
-            {/* <div style={{width: "100%", height: "50px"}} className='background-color-grey grid-5-column'></div> */}
         </header>
     )
 }
