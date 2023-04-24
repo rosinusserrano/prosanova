49c2bb3f9cf1111c28c0527d69c12a1259a3162e
yaelrealdog <57135510+yaelrealdog@users.noreply.github.com>
2023-04-24 17:53:18 +0200

    Yael:Blog Footer header shop

--- src/contents/Blog/BlogComponents.js
+++ src/contents/Blog/BlogComponents.js
@@ -1,44 +1,74 @@
 import { BACKGROUNDCOLOR } from "constants";
 import React from "react";
 
-
-export function BlogLargeButton({author, title, setExpandedPostTitle, expandedPostTitle}){
-    return (
-        <div className="border-radius"
-        style={{
-            textAlign: "center",
-            backgroundColor: expandedPostTitle == title ? "transparent": BACKGROUNDCOLOR
-        }}
-        onClick={() => setExpandedPostTitle(title)}>
-        {title}<br/>{author}
-        </div>
-    )
+export function BlogLargeButton({
+  author,
+  title,
+  setExpandedPostTitle,
+  expandedPostTitle,
+}) {
+  return (
+   <div
+      className="border-radius font-size-1 "
+      style={{
+        padding: "1rem",
+        textAlign: "center",
+        marginBottom: "0.5rem",
+        backgroundColor:
+          expandedPostTitle == title ? "transparent" : BACKGROUNDCOLOR,
+      }}
+      onClick={() => setExpandedPostTitle(title)}
+    >
+      <span style={{ fontFamily: "Montiac-Italic" }}>{title}</span>
+      <br />
+      <span style={{ fontFamily: "BastardoRegular" }}>{author}</span>
+    </div>
+  );
 }
 
-export function BlogSmallButton({author, title, setExpandedPostTitle, expandedPostTitle, setBackFunction }){
-    return (
-        <div className="border-radius"
-        style={{
-            textAlign: "center",
-            backgroundColor: expandedPostTitle == title ? "transparent": BACKGROUNDCOLOR
-        }}
-        onClick={() => {
-            setExpandedPostTitle(title)
-            setBackFunction(() => () => {setExpandedPostTitle(null)})}}>
-        {title}<br/>{author}
-        </div>
-    )
+export function BlogSmallButton({
+  author,
+  title,
+  setExpandedPostTitle,
+  expandedPostTitle,
+  setBackFunction,
+}) {
+  return (
+    <div
+      className="border-radius "
+      style={{
+        marginBottom: "0.5rem",
+
+        textAlign: "center",
+        backgroundColor:
+          expandedPostTitle == title ? "transparent" : BACKGROUNDCOLOR,
+      }}
+      onClick={() => {
+        setExpandedPostTitle(title);
+        setBackFunction(() => () => {
+          setExpandedPostTitle(null);
+        });
+      }}
+    >
+      <span style={{ fontFamily: "Montiac-Italic" }}>{title}</span>
+      <br />
+      <span style={{ fontFamily: "BastardoRegular" }}>{author}</span>
+    </div>
+  );
 }
 
-
-export function BlogSection({title, contentJSX, expandedPostTitle}){
-    return (
-        <div style={{
-            gridArea: "overlay",
-            display: expandedPostTitle == title ? "block": "none",
-        }}
-        className="background-color-grey border-radius">
-            {contentJSX}
-        </div>
-    )
+export function BlogSection({ title, contentJSX, expandedPostTitle }) {
+  return (
+    <div
+      style={{
+        gridArea: "overlay",
+        display: expandedPostTitle == title ? "block" : "none",
+        padding: "1rem 2rem",
+        fontFamily: "BastardoRegular",
+      }}
+      className="background-color-grey border-radius font-size-1 scrollable-element "
+    >
+      {contentJSX}
+    </div>
+  );
 }
---
+++ src/contents/shop.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../styles/imprint.css";
+import "../styles/fonts.css";
+import "../styles/pages.css";
+import { Helmet } from "react-helmet";
+
+function Shop() {
+  return (
+    <>
+      <Helmet>
+        <title>PROSANOVA 2023 | Info</title>
+        <meta
+          name="description"
+          content="Prosanova 2023, Festival für junge Literatur | Impressum"
+        />
+      </Helmet>
+
+      <div className="inside-main-content-wrapper" style={{
+       display: 'grid',
+        height: "100%",
+        placeItems: 'center'
+      }}>
+       <div style={{display: 'block',}}>
+        <div style={{margin: '1rem',}} className="font-size-1 background-color-grey shopButtons" >Early Bird Tickets</div>
+        <div style={{margin: '1rem', textAlign: 'center',}}  className="font-size-1 background-color-grey shopButtons" >Festival Pass</div>
+
+        
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Shop;
