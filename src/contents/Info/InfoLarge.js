5c583a82f5d9d7785aeb559fbf356f06f673b5b1
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2023-04-25 20:16:22 +0200

    tesweise sticker in desktop

--- src/contents/Info/InfoLarge.js
+++ src/contents/Info/InfoLarge.js
@@ -3,26 +3,35 @@ import { InfoBubble1Text, InfoBubble2Text } from './InfoInfo'
 
 function InfoLarge() {
     return (
-        <div className="inside-main-content-wrapper" style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "1rem",
-            height: "100%"
-        }}>
-            <div className="inside-page-padding background-color-grey border-radius font-size-5 scrollable-element"
-                style={{
-                    flexGrow: "revert",
-                }}>
-                {InfoBubble1Text}
+        <>
+            <div className="inside-main-content-wrapper" style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "1rem",
+                height: "100%"
+            }}>
+                <div className="inside-page-padding background-color-grey border-radius font-size-5 scrollable-element"
+                    style={{
+                        flexGrow: "revert",
+                    }}>
+                    {InfoBubble1Text}
+                </div>
+                <div className="inside-page-padding border-radius font-size-5 scrollable-element background-color-grey"
+                    style={{
+                        flexGrow: "revert",
+                    }}>
+                    {InfoBubble2Text}
+                    <div style={{
+                    }}>
+                        <img style={{
+                            width: "15%",
+                            rotate: "20deg",
+                            marginLeft: "75%"
+                        }} src='images/Schlechte_Woerter_Sticker.gif' />
+                    </div>
+                </div>
             </div>
-
-            <div className="inside-page-padding border-radius font-size-5 scrollable-element background-color-grey"
-                style={{
-                    flexGrow: "revert",
-                }}>
-                {InfoBubble2Text}
-            </div>
-        </div>
+        </>
     )
 }
 
