c84af6a113c1c8bb0472aae22f487e6c88cb24f5
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2023-06-21 17:09:31 +0200

    programm pdf mobile vergessen ups

--- src/contents/Programm/components_small/ProgrammSmallTagChoice.js
+++ src/contents/Programm/components_small/ProgrammSmallTagChoice.js
@@ -73,20 +73,36 @@ function ProgrammSmallTagChoice({ setDayFunction, setBackFunction }) {
                 <span className='font-b-bold'>Sonntag</span><br />
                 <span >25.06.2023</span>
             </div>
-            {/* <div className='border-radius background-color-grey'
-                onPointerOver={() => setHoveredButton("pdf")}
+            <div className='border-radius background-color-grey'
+                onPointerOver={() => setHoveredButton("programpdf")}
                 onPointerLeave={() => setHoveredButton("")}
+                onClick={() => window.open(`/Timetable_PN23.pdf`, "_blank")}
                 style={{
                     textAlign: "center",
                     padding: "0.5rem 3rem",
                     cursor: "url(MouseHand-Klick_small.png) 32 0, auto",
-                    backgroundColor: hoveredButton === "pdf" ? "transparent" : BACKGROUNDCOLOR,
+                    backgroundColor: hoveredButton === "programpdf" ? "transparent" : BACKGROUNDCOLOR,
+                    maxWidth: "15rem",
+                    marginTop: "1rem"
+                }}>
+                <span >Programmheft als PDF herunterladen</span>
+
+            </div>
+            <div className='border-radius background-color-grey'
+                onPointerOver={() => setHoveredButton("timetablepdf")}
+                onPointerLeave={() => setHoveredButton("")}
+                onClick={() => window.open(`/Programmheft_PN23.pdf`, "_blank")}
+                style={{
+                    textAlign: "center",
+                    padding: "0.5rem 3rem",
+                    cursor: "url(MouseHand-Klick_small.png) 32 0, auto",
+                    backgroundColor: hoveredButton === "timetablepdf" ? "transparent" : BACKGROUNDCOLOR,
                     maxWidth: "15rem",
                     marginTop: "1rem"
                 }}>
                 <span >Timetable als PDF herunterladen</span>
 
-            </div> */}
+            </div>
         </div>
     )
 }
