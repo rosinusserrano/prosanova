45d06a07048274aad0639ec8ebdab4de75cc3fa3
Marco Rosinus Serrano <marcorosinusserrano@gmail.com>
2023-06-24 18:56:29 +0200

    mobile landing während festival issue fix

--- src/contents/Landing/LandingSmall.js
+++ src/contents/Landing/LandingSmall.js
@@ -1,11 +1,11 @@
 import FridgePoetry from 'components/FridgePoetry'
 import React from 'react'
 
-function LandingSmall({color}) {
+function LandingSmall({ color }) {
 
     const today = new Date()
     const festivalDate = new Date("2023-06-23")
-    const daysDiff = Math.round(((festivalDate - today) / 1000 / 60 / 60 / 24) + 1)
+    const daysDiff = Math.round(((festivalDate - today) / 1000 / 60 / 60 / 24) + 1) - 0
 
     return (
         <div className='inside-main-content-wrapper border-radius background-color-grey inside-page-padding font-size-1'
@@ -20,11 +20,27 @@ function LandingSmall({color}) {
                 gap: "0.5rem",
                 width: "100%"
             }}>
-                <FridgePoetry color="white">noch</FridgePoetry>
-                <FridgePoetry color={color}>{daysDiff}</FridgePoetry>
-                <FridgePoetry color="white">Tage</FridgePoetry>
-                <FridgePoetry color="white">bis</FridgePoetry>
-                <FridgePoetry color="white">zum</FridgePoetry>
+                {daysDiff > 0 ? <>
+                    <FridgePoetry color="white">noch</FridgePoetry>
+                    <FridgePoetry color={color}>{daysDiff}</FridgePoetry>
+                    <FridgePoetry color="white">Tage</FridgePoetry>
+                    <FridgePoetry color="white">bis</FridgePoetry>
+                    <FridgePoetry color="white">zum</FridgePoetry>
+                </> : (
+                    daysDiff > -3 ? <>
+                        <FridgePoetry color="white">Der</FridgePoetry>
+                        <FridgePoetry color={color}>{(daysDiff * -1) + 1}.</FridgePoetry>
+                        <FridgePoetry color="white">Tag</FridgePoetry>
+                        <FridgePoetry color="white">des</FridgePoetry>
+                        <FridgePoetry color="white">PROSANOVA</FridgePoetry>
+                    </> : <>
+                        <FridgePoetry color={color}>{(daysDiff * -1) - 2}</FridgePoetry>
+                        <FridgePoetry color="white">{(daysDiff * -1) - 2 > 1 ? "Tage" : "Tag"}</FridgePoetry>
+                        <FridgePoetry color="white">seit</FridgePoetry>
+                        <FridgePoetry color="white">dem</FridgePoetry>
+                        <FridgePoetry color="white">PROSANOVA</FridgePoetry>
+                    </>
+                )}
                 <FridgePoetry color="white">Festival</FridgePoetry>
                 <FridgePoetry color="white">für</FridgePoetry>
                 <FridgePoetry color="white">junge</FridgePoetry>
